549bf3d57a48bbd08e73ce257cda63d9372d8960
P-JSL <althsuwpfl@gmail.com>
2023-08-20 14:53:12 +0900

    초기 커밋.

---
+++ PacktLibrary/Person.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using static System.Console;
+
+namespace Packt.Shared;
+
+public class Person : Object
+{
+    //fields
+    public string? Name;
+    public DateTime DateOfBirth;
+    public List<Person> Children = new();
+
+    //methods
+    public void WriteToConsole()
+    {
+        WriteLine($"{Name} was born on a {DateOfBirth:dddd}");
+    }
+
+    //정적메서드 (static 메서드)
+    public static Person Procreate(Person p1, Person p2)
+    {
+        Person baby = new()
+        {
+            Name = $"Baby of {p1.Name} and {p2.Name}"
+        };
+        p1.Children.Add(baby);
+        p2.Children.Add(baby);
+        return baby;
+    }
+
+    //instance method
+    public Person ProcreateWith(Person partner)
+    {
+        return Procreate(this, partner);
+    }
+
+    // * 연산자
+    public static Person operator *(Person p1, Person p2)
+    {
+        return Person.Procreate(p1, p2);
+    }
+
+    //local method
+    public static int Factorial(int number)
+    {
+        if(number < 0)
+        {
+            throw new ArgumentException($"{nameof(number)} cannot be less than zero");
+        }
+        return localFactorial(number);
+
+        int localFactorial(int localNumber)
+        {
+            if (localNumber == 1) return 1;  
+            return localNumber * localFactorial(localNumber - 1);
+            
+        }
+    }
+
+    //delegate 델리게이트 사용
+    //이벤트 델리게이트 필드
+    public event EventHandler? Shout;
+
+    //데이터 필드
+    public int AngerLevel;
+
+    //method
+    public void Poke()
+    {
+        AngerLevel++;
+        if(AngerLevel >= 3)
+        {
+            /*
+            if(Shout != null)
+            {
+                Shout(this, EventArgs.Empty);
+            }
+            */
+            Shout?.Invoke(this,EventArgs.Empty);
+        }
+    }
+
+    
+
+}
---
+++ PeopleApp/Program.cs
@@ -0,0 +1,47 @@
+﻿using Packt.Shared;
+using static System.Console;
+
+namespace PeopleApp;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Person harry = new() { Name = "Harry" };
+        Person mary = new() { Name = "Mary" };
+        Person jill = new() { Name = "Jill" };
+
+        //instance method call.
+        Person baby1 = mary.ProcreateWith(harry);
+        baby1.Name = "Gary";
+
+        //static method call
+        Person baby2 = Person.Procreate(harry,jill);
+        //static method call
+        Person baby3 = harry * mary;
+
+        WriteLine($"{harry.Name} has {harry.Children.Count} children.");
+        WriteLine($"{mary.Name} has {mary.Children.Count} children.");
+        WriteLine($"{jill.Name} has {jill.Children.Count} children.");
+        WriteLine(
+            format:"{0}`s first child is named \"{1}\".",
+            arg0:harry.Name,
+            arg1: harry.Children[0].Name
+            );
+
+        WriteLine($"5! is {Person.Factorial(5)}");
+
+        static void Harry_Sshout(object? sender, EventArgs e)
+        {
+            if (sender is null) return;
+            Person p = (Person)sender;
+            WriteLine($"{p.Name} is this angry : {p.AngerLevel}.");
+        }
+
+        harry.Shout += Harry_Sshout;
+        harry.Poke();
+        harry.Poke();
+        harry.Poke();
+        harry.Poke();
+    }
+}
